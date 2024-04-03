8d2e63e3c5f64cdc4a962a95da9046095ba86a98
David <david@taiko.xyz>
2024-04-03 21:34:34 +0800

    feat(protocol): add `SetGuardians` script

---
+++ packages/protocol/script/SetGuardians.s.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import "../test/DeployCapability.sol";
+import "../contracts/L1/gov/TaikoTimelockController.sol";
+import "../contracts/L1/provers/Guardians.sol";
+
+contract SetGuardians is DeployCapability {
+    uint256 public privateKey = vm.envUint("PRIVATE_KEY");
+    address public timelockAddress = vm.envAddress("TIMELOCK_ADDRESS");
+    address public guardianProver = vm.envAddress("GUARDIAN_PROVER");
+    address[] public guardians = vm.envAddress("GUARDIANS", ",");
+
+    function run() external {
+        require(guardians.length != 0, "invalid guardians");
+
+        vm.startBroadcast(privateKey);
+
+        setGuardiansByTimelock(timelockAddress);
+
+        vm.stopBroadcast();
+    }
+
+    function setGuardiansByTimelock(address timelock) internal {
+        bytes32 salt = bytes32(block.timestamp);
+
+        bytes memory payload = abi.encodeCall(Guardians.setGuardians, (guardians, 3));
+
+        TaikoTimelockController timelockController = TaikoTimelockController(payable(timelock));
+
+        timelockController.schedule(guardianProver, 0, payload, bytes32(0), salt, 0);
+
+        timelockController.execute(guardianProver, 0, payload, bytes32(0), salt);
+
+        for (uint256 i; i < guardians.length; ++i) {
+            console2.log("New guardian prover added:");
+            console2.log("index: ", i);
+            console2.log("instance: ", guardians[0]);
+        }
+    }
+}
